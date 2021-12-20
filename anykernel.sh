787ca90c8e427a09b2b800ac800b27b87b9165b7
Wahhap <wahhap10@gmail.com>
2021-12-20 22:58:57 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-#kernel.string=JFla-Kernel By DirtyGEN
+#kernel.string=JFla-Kernel By #PapiLer
 do.devicecheck=1
 do.modules=0
 do.systemless=1
