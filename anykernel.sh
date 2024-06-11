f28907fa23ec944bf04915f11b7e7f28034792ca
Papizola <120310298+Papi64s@users.noreply.github.com>
2024-06-12 04:42:40 +0700

    Sapphire

--- anykernel.sh
+++ anykernel.sh
@@ -10,38 +10,45 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=onc
-device.name2=onclite
+device.name1=sapphire
+device.name2=sapphiren
 device.name3=
 device.name4=
 device.name5=
-supported.versions=10-14
-supported.patchlevels=
-supported.vendorpatchlevels=
+supported.versions=
 '; } # end properties
 
-### AnyKernel install
-## boot files attributes
+## AnyKernel install
 boot_attributes() {
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+slot=$(find_slot);
+block=/dev/block/bootdevice/by-name/boot$slot;
+is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+# AnyKernel install
+split_boot;
 
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-## end boot install
+## Selinux Permissive
+#patch_cmdline androidboot.selinux androidboot.selinux=permissive
 
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+flash_boot;
+## end install
+
+sleep 3
+ui_print "kernel installed successfully"
 
 ## init_boot files attributes
 #init_boot_attributes() {
